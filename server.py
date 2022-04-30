2091586d65da0e396f702c88d810cac0c96c7564
Kbalazs3 <kbalazs93@gmail.com>
2022-04-30 18:03:01 +0200

    Index page is started

--- server.py
+++ server.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template
 app = Flask('Psychologist_Site')
 
 
+
+
+@app.route('/')
+def index_page():
+    return render_template("index.html")
+
+
 def main():
     app.run(
         debug=True
