2c9741377eb4bedc17c1417c046e276262779285
Kbalazs3 <kbalazs93@gmail.com>
2022-07-05 19:44:06 +0200

    introduction route created

--- server.py
+++ server.py
@@ -8,6 +8,14 @@ def index_page():
     return render_template("index.html")
 
 
+@app.route("/introduction")
+def 
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+
 def main():
     app.run(
         debug=True
